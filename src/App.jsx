15fec63d044f8a322afe2b898f32593fb71a16af
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2025-05-16 20:50:36 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-green-500 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
